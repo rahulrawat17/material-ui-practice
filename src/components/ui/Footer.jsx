474f3b7564a6d23201fbbbb39ca88ddcaa7d77c0
Rahul Rawat <rahul24920@gmail.com>
2021-12-15 09:41:28 +0530

    minor

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -5,10 +5,19 @@ import footerAdornment from '../../assets/Footer Adornment.svg'
 const useStyles = makeStyles(theme => ({
     footer: {
         backgroundColor: "blue",
-        width: "100%"
+        width: "100%",
+        zIndex: 1320,
+        position: "relative"
     },
     adornment: {
-        
+        width: "25em",
+        verticalAlign: "bottom",
+        [theme.breakpoints.down("md")]:{
+            width: "21em"
+        },
+        [theme.breakpoints.down("xm")]: {
+            width: "15em"
+        }
     }
 }))
 
